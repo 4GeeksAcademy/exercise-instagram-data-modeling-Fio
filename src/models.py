ed94ced5a35b02d78ff9d2d73178dbb6f371f916
Fiorella-vq <fio.89@hotmail.com>
2024-11-18 00:52:50 +0000

    parte de

--- src/models.py
+++ src/models.py
@@ -7,26 +7,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+# usuarios de Instagram con un nombre de usuario único, email y contraseña
+class User (Base): 
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column (String(250), nullable= False)
+    password = Column (String(100), nullable = False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# Comentarios.
+class Comment(Base):
+    __tablename__ = 'comments'
+    
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    content = Column(String(500), nullable= False)
+   # create_date = Column(DataTime, default=datatime.utcnow)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'), nullable=False)
 
-    def to_dict(self):
-        return {}
+
+ # Publicación de los users , hay descripción , imágen , la relación con el user  
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True) 
+    description = Column(String(255))
+    image_url = Column(String(255))
+     # create_date = Column(DataTime, default=datatime.utcnow)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+
+
+
+class Likes (Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True) 
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+
+
+
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
